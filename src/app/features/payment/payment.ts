da4661b72c9fff0609b26a7019347d07e3605ed7
Santiago Arbelaez Contreras <santiago.arbelaezc@uqvirtual.edu.co>
2026-05-27 12:43:52 -0500

    añadiendo componente de pagos

---
+++ src/app/features/payment/payment.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-payment',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './payment.html'
+})
+export class Payment {
+  route = 'Armenia → Medellín';
+  total = '$55.000';
+  date = 'Hoy, 15 Abr';
+  seat = 'A3';
+
+  methods = [
+    { title: 'Tarjeta de Crédito/Débito', icon: 'credit_card' },
+    { title: 'PSE', icon: 'account_balance' },
+    { title: 'Nequi', icon: 'phone_android' }
+  ];
+
+  selectedMethodIndex = 0;
+  isProcessing = false;
+
+  constructor(private router: Router) {}
+
+  navigateBack() {
+    this.router.navigate(['/seat-selection']);
+  }
+
+  selectMethod(index: number) {
+    this.selectedMethodIndex = index;
+  }
+
+  pay() {
+    this.isProcessing = true;
+    setTimeout(() => {
+      this.isProcessing = false;
+      this.router.navigate(['/tickets']); // Simulate success
+    }, 2000);
+  }
+}
