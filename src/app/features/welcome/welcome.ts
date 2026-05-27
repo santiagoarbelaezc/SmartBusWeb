f78be31977c95861a65b9357b6c4a79b95780c0e
Santiago Arbelaez Contreras <santiago.arbelaezc@uqvirtual.edu.co>
2026-05-27 10:48:16 -0500

    correcta configuracion

--- src/app/features/welcome/welcome.ts
+++ src/app/features/welcome/welcome.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { UiButton } from '../../shared/components/ui-button/ui-button';
 
 @Component({
   selector: 'app-welcome',
-  imports: [],
-  templateUrl: './welcome.html',
-  styleUrl: './welcome.css',
+  standalone: true,
+  imports: [CommonModule, UiButton],
+  templateUrl: './welcome.html'
 })
-export class Welcome {}
+export class Welcome {
+  constructor(private router: Router) {}
+
+  navigateToLogin() {
+    console.log('Login clicked');
+    // Implement auth later, just go to home for now to test flow
+    this.router.navigate(['/home']);
+  }
+
+  navigateToRegister() {
+    console.log('Register clicked');
+    this.router.navigate(['/home']);
+  }
+
+  googleSignIn() {
+    console.log('Google Sign-In clicked');
+    this.router.navigate(['/home']);
+  }
+}
