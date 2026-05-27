9afff694342c9c3bb786ffcd94cd87f5a64eedfb
Santiago Arbelaez Contreras <santiago.arbelaezc@uqvirtual.edu.co>
2026-05-27 10:45:58 -0500

    añadiendo rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+  { path: 'welcome', loadComponent: () => import('./features/welcome/welcome').then(m => m.Welcome) },
+  { path: 'home', loadComponent: () => import('./features/home/home').then(m => m.Home) },
+  { path: 'search', loadComponent: () => import('./features/search/search').then(m => m.Search) },
+  { path: 'seat-selection', loadComponent: () => import('./features/seat-selection/seat-selection').then(m => m.SeatSelection) },
+  { path: '**', redirectTo: 'welcome' }
+];
