d3f05ae45ec763fc765351a0e668054f1e9ebef0
Santiago Arbelaez Contreras <santiago.arbelaezc@uqvirtual.edu.co>
2026-05-27 10:43:35 -0500

    añadiendo nuevas pantallas

---
+++ src/app/features/seat-selection/seat-selection.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seat-selection',
+  imports: [],
+  templateUrl: './seat-selection.html',
+  styleUrl: './seat-selection.css',
+})
+export class SeatSelection {}
